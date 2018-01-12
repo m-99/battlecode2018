6215adf62e82154f3e3bb3820ef56336656383dc
m-99 <sammiller1999@hotmail.com>
2018-01-12 00:15:40 -0500

    created a test player class that hopefully replicates workers around the initial workers

---
+++ player-pear/Player.java
@@ -0,0 +1,23 @@
+import bc.*;
+
+public class Player {
+    public static void main(String[] args) {
+        GameController gc = new GameController();
+
+        while (true) {
+            VecUnit units = gc.units();
+
+            for (int i = 0; i < units.size(); i++) {
+                Unit unit = units.get(i);
+                if (unit.unitType().equals(UnitType.Worker)) {
+                    for (Direction direction : Direction.values()) {
+                         if (gc.canMove(unit.id(), direction) && gc.karbonite() >= 15) {
+                             gc.replicate(unit.id(), direction);
+                         }
+                    }
+                }
+            }
+            gc.nextTurn();
+        }
+    }
+}
