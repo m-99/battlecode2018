0112ae7434585e4cc4f7115a1df177b19ce83d89
m-99 <sammiller1999@hotmail.com>
2018-01-14 17:46:24 -0500

    created Tasks enum

---
+++ player-banana/Tasks.java
@@ -0,0 +1,14 @@
+public enum Tasks {
+    //general non-UnitType specific things
+    MOVE, RANDOM_MOVE, ATTACK,
+    //worker things
+    BLUEPRINT, BUILD, REPAIR, REPLICATE,
+    //knight things
+    JAVELIN,
+    //(st)ranger things
+    SNIPE,
+    //mage things
+    BLINK,
+    //healer things
+    HEAL, OVERCHARGE
+}
