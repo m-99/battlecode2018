d64373229934e49a2b83193f603f04a3740687c7
Abby Lambert <abby.lambert7899@gmail.com>
2018-01-14 17:54:20 -0500

    created a useless class

---
+++ player-banana/Target.java
@@ -0,0 +1,14 @@
+import bc.*;
+
+public class Target {
+
+        private Tasks tasks;
+        private MapLocation location;
+
+        public Target(Tasks t, MapLocation l) {
+                tasks = t;
+                location = l                                                              
+        }
+
+        
+}
