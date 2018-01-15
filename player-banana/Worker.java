4bffbed1dec581234bbe80e50a67da0debcd013d
Abby Lambert <abby.lambert7899@gmail.com>
2018-01-15 16:01:57 -0500

    created active abilities (no longer abstract) to each robot type

--- player-banana/Worker.java
+++ player-banana/Worker.java
@@ -1,5 +1,10 @@
 import bc.*;
 
-public class Worker extends Robot {
-        
+public static class Worker extends Robot {
+    public static void ability(GameController gc, int id, Direction direction) {
+    	//replication
+     	if(gc.canReplicate(id, direction) && gc.karbonite() > 15) {
+       		gc.replicate(id, direction);
+       	}
+    }
 }
